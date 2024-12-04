89dac04eb5bec4395cd41fb2469b7f6f145df1d2
unknown <developer@alexmgullen.ca>
2024-12-03 23:07:42 -0500

    day3 part2

--- 2024/day3/__main__.py
+++ 2024/day3/__main__.py
@@ -19,7 +19,59 @@ def part1(value):
     return total
 
 def part2(value):
-    pass
+
+
+    ''' Oh christmas trie, Oh christmas trie, how lovely are your... branch'''
+    christmas_trie = {
+            'd': { 
+                  'o' : {
+                        '(' : {
+                                ')' : {
+                                        'end' : 1,
+                                    }
+                            },
+                        'n' : { 
+                               '\'' : { 
+                                       't' : { 
+                                              '(' : { 
+                                                     ')' : {
+                                                            'end': 0
+                                                         }
+                                                     }
+                                              }
+                                       }
+                               }
+                        }
+                  }
+            }
+
+    do = True
+
+    r = 0
+
+    current = {}
+
+    positive = ''
+
+    while r < len(value):
+
+        if value[r] in current:
+            current = current[value[r]]
+
+        elif 'end' in current:
+            do = current['end']
+        else:
+            current = {}
+
+        if value[r] in christmas_trie:
+            current = christmas_trie[value[r]]
+
+        if do:
+            positive += value[r]
+
+        r += 1
+
+    return part1(positive)
 
 
 if __name__ == '__main__':
@@ -32,11 +84,24 @@ if __name__ == '__main__':
     input_value = input_file.read()
     input_file.close()
 
-    print(test_value)
+    print(f'test data: {test_value}')
 
     print(f'part1 test {part1(test_value)}')
 
     print(f'part1 solution {part1(input_value)}')
+
+    test_file = open('test2.txt','r')
+    test_value = test_file.read()
+    test_file.close()
+
+    print(f'test data: {test_value}')
+
+    print(f'part2 test {part2(test_value)}')
+
+
+    print(f'part2 solution {part2(input_value)}')
+
+
 
 
 
