6f96360e8731df1eb8d38a848905b2d4c11a2fa2
unknown <developer@alexmgullen.ca>
2024-12-08 10:56:30 -0500

    day7 part2

--- 2024/day7/__main__.py
+++ 2024/day7/__main__.py
@@ -24,7 +24,6 @@ def recurse(values,stack,y):
 def part1(tests):
     total = 0
     for test in tests:
-        #print(f"finding: {test[0]}")
         stack = [test[1][0]]
 
 
@@ -33,6 +32,24 @@ def part1(tests):
     return total
 
 
+def recurse2(values,stack,y):
+    if len(stack) == len(values) and stack[-1] == y:
+        return 1
+    if len(stack) >= len(values) or stack[-1] > y:
+        stack.pop()
+        return 0
+    if recurse2(values,stack + [stack[-1] + values[len(stack)]],y) or recurse2(values,stack + [stack[-1] * values[len(stack)]],y) or recurse2(values,stack + [int(str(stack[-1]) + str(values[len(stack)]))],y):
+        return 1
+    return 0
+
+
+def part2(tests):
+    total = 0
+    for test in tests:
+        stack = [test[1][0]]
+        if recurse2(test[1],stack,test[0]):
+            total += test[0]
+    return total
 
 
 if __name__ == "__main__":
@@ -50,4 +67,9 @@ if __name__ == "__main__":
     print(f"part1 test: {part1(format(test_data))}")
 
     print(f"part1 solution: {part1(format(input_data))}")
+
+
+    print(f"part2 test: {part2(format(test_data))}")
+
+    print(f"part2 solution: {part2(format(input_data))}")
 
