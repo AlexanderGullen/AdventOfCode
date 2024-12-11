e3bc68a890af96d27c4f8be9f00f1495b8d73e1b
unknown <developer@alexmgullen.ca>
2024-12-10 19:57:34 -0500

    day10 part2

--- 2024/day10/__main__.py
+++ 2024/day10/__main__.py
@@ -38,6 +38,32 @@ def part1(puzzle):
     return total
 
 
+def part2(puzzle):
+    directions = [[-1,0],[0,-1],[0,1],[1,0]]
+    trailheads = list()
+    for y in range(len(puzzle)):
+        for x in range(len(puzzle[y])):
+            if puzzle[y][x] == 0:
+                       trailheads.append([y,x])
+    total = 0
+    for trailhead in trailheads:
+        paths = 0
+        stack = deque()
+        stack.append((0,[trailhead[0],trailhead[1]]))
+        while len(stack) > 0:
+            steps, current = stack.popleft()
+
+            if  current[0] < 0 or current[0] >= len(puzzle) or current[1] < 0 or current[1] >= len(puzzle[0]):
+                pass
+            elif steps == puzzle[current[0]][current[1]] and steps == 9:
+                total += 1
+            elif steps == puzzle[current[0]][current[1]]:
+                for d in directions:
+                    dy, dx = current[0] + d[0], current[1] + d[1]
+                    if 0 <= dy < len(puzzle) and 0 <= dx < len(puzzle[0]) and puzzle[dy][dx] == (steps +1):
+                      stack.append((steps+1,[dy,dx]))
+    return total
+
 if __name__ == "__main__":
     test_file = open("test1.txt",'r')
     test_data = test_file.read()
@@ -54,4 +80,8 @@ if __name__ == "__main__":
     print(f"part1 test: {part1(format(test_data))}")
 
     print(f"part1 solution: {part1(format(input_data))}")
+
+    print(f"part2 test: {part2(format(test_data))}")
+
+    print(f"part2 solution: {part2(format(input_data))}")
 
