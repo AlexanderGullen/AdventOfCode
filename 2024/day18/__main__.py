0bf6706c6023aa73eefd19f59886830f9c26cba5
unknown <developer@alexmgullen.ca>
2024-12-18 23:24:35 -0500

    day18 part2

--- 2024/day18/__main__.py
+++ 2024/day18/__main__.py
@@ -9,38 +9,72 @@ def part1(pixels,w,n):
     edges = defaultdict(list)
     malfunctioning=set(pixels[:n])
 
-    print(malfunctioning)
-
     #create a graph for traversal
     for y in range(w+1):
         for x in range(w+1):
             for d in directions:
                 if x + d[0] >= 0 and y + d[1] >= 0 and x + d[0] <= w and y + d[1] <= w and (x + d[0],y + d[1]) not in malfunctioning:
                     edges[(x,y)].append((x + d[0],y + d[1]))
-    #print(edges)
     
+
+    #djikstra's algorithm
     heap = []
-    visited = set()
-    nodes = {k:float("inf") for k in edges.keys()}
+    distances = dict()
     heapq.heappush(heap,(0,(0,0)))
-    nodes[(0,0)] = 0
+    distances[(0,0)] = 0
 
     while len(heap) > 0:
-        print(visited)
         current = heapq.heappop(heap)
-        visited.add(current[1])
-        #print(f"current: {current}")
-        #print(f"adjacent: {edges[current[1]]}")
+
+        if current[1] == (w,w):
+            break
+
         for edge in edges[current[1]]:
-            nodes[edge] = min(nodes[edge],nodes[current[1]] + 1)
-            if edge not in visited:
-                heapq.heappush(heap,(nodes[edge],edge))
+            new_cost = distances[current[1]] + 1
+            if edge not in distances or new_cost < distances[edge]:
+                distances[edge] = new_cost
+                heapq.heappush(heap,(new_cost,edge))
 
-    return nodes[(6,6)]
+    return distances[(w,w)]
+
+def traversable(edges,w):
+    heap = []
+    distances = dict()
+    heapq.heappush(heap,(0,(0,0)))
+    distances[(0,0)] = 0
+
+    while len(heap) > 0:
+        current = heapq.heappop(heap)
+
+        if current[1] == (w,w):
+            break
+        
+        for edge in edges[current[1]]:
+            new_cost = distances[current[1]] + 1
+            if edge not in distances or new_cost < distances[edge]:
+                distances[edge] = new_cost
+                heapq.heappush(heap,(new_cost,edge))
+
+    return True if (w,w) in distances else False
 
 
+def part2(pixels,w):
+    n = 0
 
+    while True:
+        directions = [(1,0),(-1,0),(0,1),(0,-1)]
+        edges = defaultdict(list)
+        malfunctioning=set(pixels[:n])
 
+        #create a graph for traversal
+        for y in range(w+1):
+            for x in range(w+1):
+                for d in directions:
+                    if x + d[0] >= 0 and y + d[1] >= 0 and x + d[0] <= w and y + d[1] <= w and (x + d[0],y + d[1]) not in malfunctioning:
+                        edges[(x,y)].append((x + d[0],y + d[1]))
+        if not traversable(edges,w):
+            return pixels[n - 1]
+        n += 1
 
 
 if __name__ == "__main__":
@@ -48,12 +82,17 @@ if __name__ == "__main__":
     test_data = test_file.read()
     test_file.close()
 
-    print(f"part1 format: {format(test_data)}")
-
-    print(f"part1 test: {part1(format(test_data),6,12)}")
-
     input_file = open("input1.txt",'r')
     input_data = input_file.read()
     input_file.close()
 
+
+    print(f"part1 format: {format(test_data)}")
+
+    print(f"part1 test: {part1(format(test_data),6,12)}")
+
     print(f"part1 solution: {part1(format(input_data),70,1024)}")
+
+    print(f"part2 test: {part2(format(test_data),6)}")
+
+    print(f"part2 solution: {part2(format(input_data),70)}")
